4355981327c8fec1c2dfcb41990622665d517bc8
hinkalinka <100852420+hinkalinka@users.noreply.github.com>
2023-06-10 02:22:05 +0300

    Add css folder and mod-y news.php

---
+++ news.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang=ru>
+	<head>
+		<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <script src="js/main.js"></script>
+		<title>Новости</title>
+	</head>
+<body>
+	<header class="">
+		
+	</header>
+	<section class="news">
+		<div class="news-block">
+			<div class="news-out">
+        
+      </div>
+      <div class="paga_list" align="center">
+      	<a href="#"><button class="page_button">1</button></a>
+      </div>
+		</div>
+	</section>
+	<footer class="">
+		
+	</footer>
+</body>
