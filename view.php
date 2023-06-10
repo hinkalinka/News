d0e841a182c97ab8f8e146a5b523a1a82ca0809c
hinkalinka <100852420+hinkalinka@users.noreply.github.com>
2023-06-10 08:37:17 +0300

    Конец верстки страницы view.php

--- view.php
+++ view.php
@@ -13,7 +13,7 @@ $news = R::findOne('news', 'id = ?', array($_GET['id']));
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/normalize.css">
-		<title>Новости</title>
+		<title><?php echo $news->title; ?></title>
 	</head>
 <body>
 	<header class="">
@@ -21,14 +21,14 @@ $news = R::findOne('news', 'id = ?', array($_GET['id']));
 	</header>
 
 		<section class="news_cover">
-			<p class="title_news"><?php echo $news->title; ?></p>
-		  <p class="announce_news"><?php echo $news->announce; ?></p>
-		  <p class="content_news"><?php echo $news->content; ?></p>
-		  <p class="announce_idate"><?php echo date("Y-m-d", $news->idate); ?></p>
+			<h1 class="title_news_view"><?php echo $news->title; ?></h1>
+		  <h2 class="announce_news_view"><?php echo $news->announce; ?></h2>
+		  <h3 class="content_news_view"><?php echo $news->content; ?></h3>
+		  <h5 class="announce_idate_view"><?php echo date("Y-m-d", $news->idate); ?></h5>
 		</section>
 
-	<footer class="">
-		
+	<footer>
+		<h2 class="back"><a href="news.php">Все новости</a></h2>
 	</footer>
 </body>
 </html>
