f596c52000352c53e3077b5cc88cf78ea9f0a16a
hinkalinka <100852420+hinkalinka@users.noreply.github.com>
2023-06-10 06:47:22 +0300

    удаление и изменение сортировки

--- Php/db.php
+++ Php/db.php
@@ -3,15 +3,6 @@ require "lib/rb.php";
 
 R::setup('mysql:host=localhost;dbname=news','root','');
 
-function get_time($id){
-	$time = R::findAll('news');
-	$all_time = array();
 
-	foreach ($time as $row) {
-		if ($row['idate'] == $id) {
-				$all_time[] = $row;
-		}
-	}
-}
 
 ?>
