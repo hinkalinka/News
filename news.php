05572018a379b0a7caa602b64e43bbde268a2540
hinkalinka <100852420+hinkalinka@users.noreply.github.com>
2023-06-10 04:36:04 +0300

    Изменение стилей и добавление даты

--- news.php
+++ news.php
@@ -19,29 +19,32 @@ $page_count = floor(count($news) / $count);
 		<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/normalize.css">
-    <script src="js/main.js"></script>
 		<title>Новости</title>
 	</head>
 <body>
 	<header class="">
 		
 	</header>
-	<section class="news">
+	<section class="news_cover">
 		<div class="news-block">
 			<div class="news-out">
         <?php for($i = $page*$count; $i < ($page+1)*$count; $i++) :?>
-        <div class="news">
-        	<a href="/news.php?id=<?php echo $news[$i]->id; ?>">
-        		<p class="title_news"><?php echo $news[$i]->title; ?></p>
-        		<p class="announce_news"><?php echo $news[$i]->announce; ?></p>
-        	</a>
-        </div>
+	        <?php if($news[$i]->id !=NULL) :?>
+		        <div class="news">
+		        	<a href="/news.php?id=<?php echo $news[$i]->id; ?>">
+		        		<p class="title_news"><?php echo $news[$i]->title; ?></p>
+		        		<p class="announce_news"><?php echo $news[$i]->announce; ?></p>
+		        		<p class="announce_idate"><?php echo date("Y-m-d", $news[$i]->idate); ?></p>
+		        	</a>
+		        </div>
+	        <?php endif;?>
         <?php endfor; ?>
       </div>
       <div class="paga_list" align="center">
       	<?php for($p = 0; $p <= $page_count; $p++) :?>
-      	<a href="?page<?php echo $p;?>"><button class="page_button"><?php echo $p + 1; ?></button></a>
+      	<a href="news.php?page=<?php echo $p; ?>"><button class="page_button"><?php echo $p + 1; ?></button></a>
       	<?php endfor;?>
       </div>
 		</div>
